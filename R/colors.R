0663f5f9a56936f5f6868077b4e4641edc736a83
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-09-19 10:38:46 +0200

    Add tools for highcharter maps

---
+++ R/colors.R
@@ -0,0 +1,35 @@
+#' Return a list of color breaks for Higchart maps
+#'
+#' @param breaks The data with the breaks (use santoku chop !)
+#' @param palette A color palette that can take a numeric argument
+#'
+#' @return A list of lists to use in colorAxis' dataClasses argument
+#' @export
+gtl_hc_color_axis <- function(breaks, palette) {
+  lvls <- levels(breaks)
+  colors <- palette(length(lvls))
+
+  tibble::tibble(
+    lvls = lvls,
+    colors = colors
+  ) |>
+    purrr::pmap(gtl_hc_color_list) -> color_axis_list
+}
+
+#' Function to build the highchart colorAxis list
+#'
+#' @param ... lvls and colors
+#'
+#' @keywords internal
+gtl_hc_color_list <- function(...) {
+  data <- list(...)
+
+  from <- stringr::str_extract(data$lvls, "[\\[)]([^,]*),\\W([^\\])]*)[)\\]]", group = 1)
+  to <- stringr::str_extract(data$lvls, "[\\[)]([^,]*),\\W([^\\])]*)[)\\]]", group = 2)
+
+  list(
+    color = data$colors,
+    from = from,
+    to = to
+  )
+}
